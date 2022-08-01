4aa9d195a5cb2cbde6bedd219c3f5202332aa1a7
Sebastian Anioke <aniokechukwudi7@gmail.com>
2022-08-01 09:40:45 +0100

    added play variable

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
 
-// Prompt user to select a media stram
-// Pass to video element and play
+// To check if the user shared the screen or not;
+let play = false;
+// Prompt user to select a media stram and
+// pass to video element and play
 async function passMediaStream() {
+     
      try {
           const videoStream = await navigator.mediaDevices.getDisplayMedia();
           videoElement.srcObject = videoStream;
           videoElement.onloadedmetadata = () => {
                videoElement.play();
+               play = true;
+               console.log(play)
           }
      } catch(error) {
      console.log("oops, video fetch error", error)
@@ -16,13 +21,18 @@ async function passMediaStream() {
 } 
 
 button.addEventListener('click', async function() {
-     // Disable Button
-     button.disabled = true;
-     // Start Picture in Picture
-     await videoElement.requestPictureInPicture();
-     // Reset Button
-     button.disabled = false
+     if (play) {
+          // Disable Button
+          button.disabled = true;
+          // Start Picture in Picture
+          await videoElement.requestPictureInPicture();
+          // Reset Button
+          button.disabled = false
+     }else if (!play) {
+          passMediaStream();
+     }
 })
+console.log("outside", play)
 
 // On Load
 passMediaStream();
